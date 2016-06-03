8a3fe5cb303553b6a737b1b5c9f71db824704d1c
htha9587 <harrison.thacker69@gmail.com>
2016-06-03 13:56:24 -0600

    Version 0.1
    Created all files and classes.

---
+++ src/application/MainController.java
@@ -0,0 +1,13 @@
+package application;
+
+/**
+ * Version 0.1
+ * @author htha9587
+ * 6-3-16
+ */
+
+
+
+public class MainController {
+
+}
---
+++ src/application/Model.java
@@ -0,0 +1,18 @@
+package application;
+
+/**
+ * Version 0.1
+ * @author htha9587
+ * 6-3-16
+ */
+
+public class Model 
+{
+	public float calculate(long number1, long number2, String Operator)
+	{
+		
+		
+		
+		return 0;
+	}
+}
