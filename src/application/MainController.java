e8b755c7e2dfe290db12f4bc2fbd231f71cfc870
htha9587 <harrison.thacker69@gmail.com>
2016-06-03 14:36:59 -0600

    Version 0.2
    Created GUI, numbers and operators have corresponding events.

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,13 +1,24 @@
 package application;
 
+import javafx.event.ActionEvent;
+
 /**
  * Version 0.1
  * @author htha9587
  * 6-3-16
  */
 
+public class MainController 
+{
 
-
-public class MainController {
-
+	public void processNumbers(ActionEvent event)
+	{
+		
+	}
+	
+	public void processOperators(ActionEvent event)
+	{
+		
+	}
+	
 }
--- src/application/Model.java
+++ src/application/Model.java
@@ -10,9 +10,24 @@ public class Model
 {
 	public float calculate(long number1, long number2, String Operator)
 	{
+		switch(Operator)
+		{
+		case "+":
+			return number1 + number2;
+		case "-":
+			return number1 - number2;
+		case "*":
+			return number1 * number2;
+		case "/":
+			if(number2 == 0)
+			return 0;
+			return number1 / number2;
+		
+		default:
+		
+			return 0;
+		}
 		
 		
-		
-		return 0;
 	}
 }
